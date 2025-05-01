f91d7247287b8786fdb2b08d002292ebc230001e
anveshrjk <nightmarexd60@gmail.com>
2025-05-01 08:46:45 +0530

    new codes

--- src/ConverterChallenges.java
+++ src/ConverterChallenges.java
@@ -2,16 +2,53 @@
 public class ConverterChallenges {
 
 	public static void main(String[] args) {
+		//to CM 
 		System.out.println(convertToCentimeters(72) + " cm");
 		System.out.println(convertToCentimeters(5, 11) + " cm");
+		
+		//to hours
+		System.out.println(getDurationString(-3945));  // This is the first test case
+        System.out.println(getDurationString(-65, 45));  // This is the second test case
+        System.out.println(getDurationString(65, 145));
+        System.out.println(getDurationString(65, 45));
+        System.out.println(getDurationString(3945));		
 	}
+	//converts inches and feet to cm
 	public static double convertToCentimeters(int inches) {//converts from inches to cm
 		return inches * 2.54;
 	};
 	public static double convertToCentimeters(int feet, int inches) {//from feet'in to cm
 		return convertToCentimeters((feet * 12) + inches);
 	};
-	private static int convertToMeter(int cm) {
-		return convertToMeter(0);
-	}
+	//convert seconds and minutes into hours
+	public static String getDurationString(int seconds) {
+
+        if (seconds < 0) {
+            return "Invalid data for seconds(" + seconds
+                    + "), must be a positive integer value";
+        }
+
+        int minutes = seconds / 60;
+        return getDurationString(seconds / 60, seconds % 60);
+    }
+
+    public static String getDurationString(int minutes, int seconds) {
+
+        if (minutes < 0) {
+            return "Invalid data for minutes(" + minutes
+                    + "), must be a positive integer value";
+        }
+
+        if (seconds < 0 || seconds > 59) {
+            return "Invalid data for seconds(" + seconds
+                    + "), must be between 0 and 59";
+        }
+
+        int hours = minutes / 60;
+
+        int remainingMinutes = minutes % 60;
+
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+    }
+	
 }
--- src/Exercise.java
+++ src/Exercise.java
@@ -1,16 +1,22 @@
 public class Exercise {//exercise code will be saved here
+	//useful c&p
+	//System.out.println("-------------------------------------------------------------------");
+	//System.out.println();
+	
 	public static void main(String[]args) {//will call the methods exercise-wise~
 		
-		// *TOKEN* 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
-		// *TOKEN-anveshrjk* 
+		// *TOKEN* - anveshrjk: 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
 		
 		//Exercise 1 
-		checkNumber(-4);
+		checkNumber(0);
+		checkNumber(38);
+		checkNumber(-38);
+		System.out.println("-----------------------------------------------------------------");
 		
 		//Exercise 2 
 		printAns(15.38);
-        printAns(-5.653);
         printAns(10548.944569);
+        printAns(-5.653);
         
         //Exercise 3
         printKBtoMB(2048);
@@ -18,34 +24,77 @@ public class Exercise {//exercise code will be saved here
         
         //Exercise 4
         shouldWakeUp(false, 0);
+        
+        //Exercise 5
+        System.out.println(isLeapYear(2005));
+        System.out.println(isLeapYear(2020));
+        System.out.println("------------------------------------------------------------------");
+        
+        //Exercise 6
+        System.out.println(areEqualByThreeDecimalPlaces(3.145,3.1458));
+        System.out.println(areEqualByThreeDecimalPlaces(3.145,3.1478));
+        System.out.println("--------------------------------------------------------------------");
+       
+        //Exercise 7
+        System.out.println(hasEqualSum(1,2,3));
+        System.out.println(hasEqualSum(3,2,1));
+        System.out.println("---------------------------------------------------------------------");
+        
+        //Exercise 8
+        System.out.println(hasTeen(1, 19, 40));
+        System.out.println(hasTeen(12, 7, 6));
+        System.out.println("----------------------------------------------------------------------");
+        
+        //Exercise 9
+        System.out.println(area(7.35633));
+        System.out.println(area(7.837345,14.3543));
+        System.out.println("-----------------------------------------------------------------------");
+        
+        //Exercise 10
+        printYearsAndDays(10518984);
+        printYearsAndDays(9993035);
+        System.out.println("-------------------------------------------------------------------");
+        
+        //Exercise 11
+        printEqual(7, -1, 5);
+        printEqual(1, 1, 1);
+        printEqual(2, 2, 7);
+        printEqual(7, 8, 9);
+        System.out.println("-------------------------------------------------------------------");
 	}
-//Exercise 1 -----------------------------------------------------------------------------------------------------
+//Exercise 1 ---------------------------------------------------------------------------------------
 	public static void checkNumber(int number){
-        System.out.println((number>0)  ? "positive\n" : ((number<0) ? "negative\n" : "zero\n"));
+		
+        System.out.println((number>0)  ? "positive" : ((number<0) ? "negative" : "zero"));
     }
 
-//Exercise 2 -----------------------------------------------------------------------------------------------------
+//Exercise 2 ---------------------------------------------------------------------------------------
 	public static long toMPH(double KMPH){
+		
         long MPH;
         if(KMPH>0){
             MPH = Math.round(KMPH/1.609);
             return MPH;
         }
         return -1;
+        
     }
     public static void printAns(double KMPH){
+    	
         if(KMPH>0){
             System.out.println(KMPH + " KMPH = " + toMPH(KMPH) + " MPH");
         }
         else{
         	System.out.println(KMPH + " is an Invalid Value");
+        	System.out.println("-----------------------------------------------------------------------------------------------------");
         }        
     }
     
-//Exercise 3 -----------------------------------------------------------------------------------------------------
+//Exercise 3 ----------------------------------------------------------------------------------------
     public static void printKBtoMB(int kiloBytes){
         
         if(kiloBytes<0){
+        	
             System.out.println("Invalid Value");
             return;
         }
@@ -56,22 +105,138 @@ public class Exercise {//exercise code will be saved here
 		System.out.println(kiloBytes+" KB = " + mb + " MB and "+ remainingKB + " KB");   
     }
     public static void printMBtoKB(float megaBytes) {
+    	
     	if(megaBytes<0) {
     		System.out.println("Invalid Value");
     	}
+    	
     	float kb = megaBytes * 1024F;
     	System.out.println(megaBytes + " MB = " + kb + " KB");
-<<<<<<< HEAD
-    	
-=======
->>>>>>> b9db98fd7dc904b08e92c5fba6734d9d7ee1b205
+    	System.out.println("-----------------------------------------------------------------------------------------------------");
     }
     
-// Exercise 4 -----------------------------------------------------------------------------------------------
+// Exercise 4 --------------------------------------------------------------------------------------
+
     public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+    	
         if(hourOfDay < 0 || hourOfDay > 23){
+        	
             return false;
         }
+        
         return barking && (hourOfDay < 8 || hourOfDay > 22);
     }
-}
+
+// Exercise 5 -------------------------------------------------------------------------------------
+
+    public static boolean isLeapYear(int year){
+        
+        if(year < 1 || year > 9999){
+        	
+            return false;           
+        }         
+        if(year % 4 == 0){
+        	
+            if(year % 100 != 0){
+            	
+                return true;                
+            }   
+            
+            else{                
+            	if(year % 400 == 0){
+            		
+                    return true;                    
+                }   
+            	
+                else {
+                	
+                     return false;                     
+                }
+            }
+        }         
+        else{            
+            return false;
+        }
+    }
+
+//Exercise 6 ---------------------------------------------------------------------------------------
+
+	public static boolean areEqualByThreeDecimalPlaces(double a, double b){
+		
+		long firstRounded = (long) (a * 1000);
+		long secondRounded = (long) (b * 1000);
+		return firstRounded == secondRounded;
+	}
+//Exercise 7 --------------------------------------------------------------------------------------
+		
+	public static boolean hasEqualSum(int a, int b, int c){
+        if(a+b == c){
+            
+            return true;
+        }
+        return false;
+    }
+		
+//Exercise 8 -------------------------------------------------------------------------------------
+	public static boolean hasTeen(int age1, int age2, int age3){
+        
+			return isTeen(age1) || isTeen(age2) || isTeen(age3);
+    	}
+    	public static boolean isTeen(int age){
+        
+    		return age > 12 && age < 20;
+    	}
+//Exercise 9 -------------------------------------------------------------------------------------
+    	
+    	public static double area(double radius){
+            if(radius < 0.0){
+                return -1.0;
+            }
+            return Math.PI * radius * radius;
+        }
+        public static double area(double x, double y){
+            if(x < 0 || y < 0){
+                
+                return -1.0;
+            }
+            return x * y;
+        }
+        
+//Exercise 10 ----------------------------------------------------------------------------------
+	
+        public static void printYearsAndDays(long minutes){
+            if(minutes < 0){
+                System.out.println("Invalid Value");
+            }
+            else{
+                long years = minutes / 525600;
+                long remainingDays = (minutes % 525600) / 1440 ;
+                System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+                
+            }
+        }
+        
+//Exercise 11 ----------------------------------------------------------------------------------
+        
+        public static void printEqual(int a, int b, int c){
+            if (a<0 || b<0 || c<0){
+                System.out.println("Invalid Value");
+            }
+            else if (a == b && a == c){
+                System.out.println("All numbers are equal");
+            }
+            else if ((a == b ) || (a ==c ) || b == c){
+                System.out.println("Neither all are equal or different");
+            } 
+            else{
+                System.out.println("All numbers are different");
+            }
+        }
+        
+//Exercise 12 ----------------------------------------------------------------------------------
+        
+        
+//Exercise 13 ----------------------------------------------------------------------------------
+        
+        
+}//class closed
